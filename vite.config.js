5d72a40c84645fe2a64f5882864977d3e9226ff3
Etienne Bömcke <etienne@awellhealth.com>
2024-04-06 12:25:38 +0200

    feat: add type definitions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts({
+    exclude: ['stories/**']
+  })],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
@@ -19,9 +23,11 @@ export default defineConfig({
         // for externalized deps
         globals: {
           react: 'React',
+          'react-dom': 'reactDom',
         },
       },
     },
+    sourcemap: true
   },
   resolve: {
     alias: [
