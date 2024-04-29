a01cbb49ef4023be18b711063855cae3ac6a8862
Abigaila Ekiert <arekiert@gmail.com>
2024-04-29 20:03:38 +0300

    add-error-dialog

---
+++ src/stories/AlertDialog.stories.tsx
@@ -0,0 +1,48 @@
+// Replace your-framework with the name of your framework
+import type { Meta, StoryObj } from '@storybook/react';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+const meta = {
+  component: AlertDialog,
+} satisfies Meta<typeof AlertDialog>;
+
+export default meta;
+
+type Story = StoryObj<typeof AlertDialog>;
+
+function AlertDialogDemo() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+
+export const Example = {
+  args: {},
+  render: () => <AlertDialogDemo />
+} satisfies Story;
