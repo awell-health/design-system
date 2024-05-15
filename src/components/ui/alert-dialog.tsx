45ff53a3b9b754daf6c1ce47da1e699847e0db30
Pawel Skroban <pskrobek@gmail.com>
2024-05-15 09:20:55 +0200

    Update dialog props

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+
+import { Icon } from '@/components/ui/Icon'
+
 interface Props {
   title: string;
   text?: string;
@@ -7,10 +10,11 @@ interface Props {
     label: string;
     buttonAction?: () => void;
   };
+  onClose?: () => void;
 }
 
 const AlertDialog = (props: Props): React.JSX.Element => {
-  const { title, text, button, children = null } = props;
+  const { title, text, button, children = null, onClose } = props;
 
   return (
     <div
@@ -23,22 +27,23 @@ const AlertDialog = (props: Props): React.JSX.Element => {
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-white p-6 pb-8">
-              <div>
-                <div>
-                  <h3
-                    className="text-base font-semibold leading-6 text-gray-900"
-                    id="modal-title"
-                  >
-                    {title}
-                  </h3>
-                  {text && (
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">{text}</p>
-                    </div>
-                  )}
+            <div className="bg-white p-6 pb-8 relative">
+              {onClose && 
+                <div className="absolute top-0 right-0 p-4 cursor-pointer text-slate-950" onClick={onClose}>
+                  <Icon icon="RiCloseLine" />
                 </div>
-              </div>
+              }
+              <h3
+                className="text-base font-semibold leading-6 text-gray-900"
+                id="modal-title"
+              >
+                {title}
+              </h3>
+              {text && (
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{text}</p>
+                </div>
+              )}
             </div>
             {children && <div className="pb-8 px-6">{children}</div>}
             {button && (
@@ -59,4 +64,4 @@ const AlertDialog = (props: Props): React.JSX.Element => {
   );
 };
 
-export { AlertDialog }
+export { AlertDialog };
