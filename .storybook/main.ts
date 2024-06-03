0daf50ad9d81c618a10aeb7184e6ab13385323a5
Pawel Skroban <pskrobek@gmail.com>
2024-06-03 15:13:23 +0200

    Add storybook docs and extensions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,5 @@
-// Replace your-framework with the framework you are using (e.g., react-vite, vue3-vite)
 import type { StorybookConfig } from '@storybook/react-vite';
+
 
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
@@ -8,11 +8,15 @@ const config: StorybookConfig = {
     // See https://storybook.js.org/docs/api/main-config-typescript for more information about this option.
     reactDocgen: 'react-docgen-typescript',
   },
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx', 
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
   async viteFinal(config, options) {
     // Add your configuration here
     return config;
   },
+  addons: ['@storybook/addon-essentials', '@storybook/addon-measure'],
 };
 
 export default config;
