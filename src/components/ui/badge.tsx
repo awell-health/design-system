34d7864e385c6a914c6ecb8f7bc4490cfad6752f
Abigaila Ekiert <44117299+sharlotta93@users.noreply.github.com>
2024-06-11 12:57:18 +0200

    Qa fixes (#54)
    * add missing icon
    * fix border color as per designs
    * v0.3.8

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,17 +8,17 @@ const badgeVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-slate-100 text-slate-800", 
-        primary: "bg-blue-100 text-blue-800",
-        error: "bg-red-100 text-red-800",
-        warning: "bg-orange-100 text-orange-800",
-        success: "bg-green-100 text-green-800",
-        yellow: "bg-yellow-100 text-yellow-800",
-        lime: "bg-lime-100 text-lime-800",
-        cyan: "bg-cyan-100 text-cyan-800",
-        teal: "bg-teal-100 text-teal-800",
-        violet: "bg-violet-100 text-violet-800",
-        pink: "bg-pink-100 text-pink-800"
+        default: "bg-slate-100 text-slate-800 border-slate-100", 
+        primary: "bg-blue-100 text-blue-800 border-blue-100",
+        error: "bg-red-100 text-red-800 border-red-100",
+        warning: "bg-orange-100 text-orange-800 border-orange-100",
+        success: "bg-green-100 text-green-800 border-green-100",
+        yellow: "bg-yellow-100 text-yellow-800 border-yellow-100",
+        lime: "bg-lime-100 text-lime-800 border-lime-100",
+        cyan: "bg-cyan-100 text-cyan-800 border-cyan-100",
+        teal: "bg-teal-100 text-teal-800 border-teal-100",
+        violet: "bg-violet-100 text-violet-800 border-violet-100",
+        pink: "bg-pink-100 text-pink-800 border-pink-100",
       },
       size: {
         sm: "badge-sm p-2",
