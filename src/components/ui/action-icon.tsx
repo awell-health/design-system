084f249f9cd3b89d01b8aa8f4ba70532a0af3198
Abigaila Ekiert <arekiert@gmail.com>
2024-06-11 17:24:10 +0100

    MR fixes

--- src/components/ui/action-icon.tsx
+++ src/components/ui/action-icon.tsx
@@ -24,59 +24,43 @@ const ActionIcon = React.forwardRef<HTMLDivElement, ActionIconProps>(
   ({ ...props }, _ref) => {
     const TYPES_ICONS_MAP: Record<
       ActionType,
-      { icon: IconType }
+      IconType
     > = {
-      [ActionType.ApiCall]: {
-        icon: "RiTerminalBoxFill"
-      },
-      [ActionType.ApiCallGraphql]: {
-        icon: "RiTerminalWindowFill"
-      },
-      [ActionType.Calculation]: {
-        icon: "RiCalculatorFill"
-      },
-      [ActionType.Checklist]: {
-        icon: "RiCheckboxMultipleFill"
-      },
-      [ActionType.ClinicalNote]: {
-        icon: "RiHealthBookFill"
-      },
-      [ActionType.Form]: {
-        icon: "RiSurveyFill"
-      },
-      [ActionType.Message]: {
-        icon: "RiMessage3Fill"
-      },
-      [ActionType.Plugin]: {
-        icon: "RiUploadCloudFill"
-      },
-      [ActionType.PushToEmr]: {
-        icon: "RiUploadCloudFill"
-      },
+      [ActionType.ApiCall]: "RiTerminalBoxFill",
+      [ActionType.ApiCallGraphql]: "RiTerminalWindowFill",
+      [ActionType.Calculation]: "RiCalculatorFill",
+      [ActionType.Checklist]: "RiCheckboxMultipleFill",
+      [ActionType.ClinicalNote]:  "RiHealthBookFill",
+      [ActionType.Form]: "RiSurveyFill",
+      [ActionType.Message]: "RiMessage3Fill",
+      [ActionType.Plugin]: "RiUploadCloudFill",
+      [ActionType.PushToEmr]: "RiUploadCloudFill"
     }
 
     const defaultIconProps = {
       icon: "RiQuestionFill"
     }
+    
+    const { icon_url, actionType } = props
 
-    const iconProps =
-      TYPES_ICONS_MAP[props.actionType as keyof typeof TYPES_ICONS_MAP] ??
+    const icon =
+      TYPES_ICONS_MAP[actionType as keyof typeof TYPES_ICONS_MAP] ??
       defaultIconProps
 
-    const color = iconProps.icon === "RiQuestionFill" ? "fill-slate-400" : "fill-slate-800";
+    const color = icon === "RiQuestionFill" ? "fill-slate-400" : "fill-slate-800";
 
-    if (props.actionType === ActionType.Plugin && props.icon_url) {
+    if (actionType === ActionType.Plugin && icon_url) {
       return (
         <img
           className='w-6 h-6 rounded-md border border-slate-200'
-          src={props.icon_url}
+          src={icon_url}
         />
       )
     }
 
     return (
       <div className="w-6 h-6 flex bg-slate-100 rounded-md border border-slate-200 items-center justify-center">
-        <Icon {...iconProps} className={cn("w-4 h-4", color)} />
+        <Icon icon={icon} className={cn("w-4 h-4", color)} />
       </div>
     )
   },
