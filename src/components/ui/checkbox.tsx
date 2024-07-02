860f4aeedc0c27c823c30887026015819b6332ca
Paweł <pskrobek@gmail.com>
2024-07-02 16:29:07 +0200

    Extend checkbox to support radio type (#88)
    * Extend checkbox to support radio type
    * Update package version

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -15,74 +15,73 @@ const checkboxVariants = cva("", {
   },
 });
 
-export interface Checkbox
+export interface CheckboxProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
     VariantProps<typeof checkboxVariants> {
   label?: string;
   disabled?: boolean;
   indeterminate?: boolean;
   helpText?: string;
+  type: "radio" | "checkbox";
 }
 
-const Checkbox = React.forwardRef<HTMLInputElement, Checkbox>(
-  ({
-    className,
-    label,
-    disabled,
-    helpText,
-    indeterminate = false,
-    inputSize,
-    ...props
-  }) => {
-    const inputRef = React.useRef<HTMLInputElement>(null);
+const Checkbox: React.FC<CheckboxProps> = ({
+  className,
+  label,
+  disabled,
+  helpText,
+  indeterminate = false,
+  inputSize,
+  type,
+  ...props
+}) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
-    React.useEffect(() => {
-      inputRef.current!.indeterminate = indeterminate;
-    }, [indeterminate]);
+  React.useEffect(() => {
+    inputRef.current!.indeterminate = indeterminate;
+  }, [indeterminate]);
 
-    const boxShadowClass: string = 'shadow-[0px_0px_0px_2px_rgb(219,234,254)]'
+  const boxShadowClass: string = "shadow-[0px_0px_0px_2px_rgb(219,234,254)]";
 
-    return (
-      <div className="flex flex-col">
-        <label className="flex items-center gap-2 cursor-pointer">
-          <input
-            type="checkbox"
-            className={cn(
-              checkboxVariants({ inputSize }),
-              "relative bg-white border border-slate-300 rounded outline-none",
-              "flex items-center justify-center",
-              "hover:bg-blue-50 hover:border-blue-600",
-              `active:border-blue-300 active:${boxShadowClass}`,
-              `focus:border-blue-300 focus:${boxShadowClass} focus:ring-transparent`,
-              "disabled:!bg-slate-100 disabled:!border-slate-200",
-              className,
-            )}
-            ref={inputRef}
-            disabled={disabled}
-            {...props}
-          />
-          {label && (
-            <span
-              className={cn(
-                "text-slate-700 text-sm font-medium leading-tight",
-                disabled && "text-slate-300",
-              )}
-            >
-              {label}
-            </span>
+  return (
+    <div className="flex flex-col">
+      <label className="flex items-center gap-2 cursor-pointer">
+        <input
+          type={type}
+          className={cn(
+            checkboxVariants({ inputSize }),
+            "relative bg-white border border-slate-300 rounded outline-none",
+            "flex items-center justify-center",
+            "hover:bg-blue-50 hover:border-blue-600",
+            `active:border-blue-300 active:${boxShadowClass}`,
+            `focus:border-blue-300 focus:${boxShadowClass} focus:ring-transparent`,
+            "disabled:!bg-slate-100 disabled:!border-slate-200",
+            type === "radio" && "rounded-full",
+            className,
           )}
-        </label>
-        {helpText && (
-          <div className="flex gap-2">
-            <div className={checkboxVariants({ inputSize })} />
-            <span className="text-slate-500 text-sm font-normal">
-              {helpText}
-            </span>
-          </div>
+          ref={inputRef}
+          disabled={disabled}
+          {...props}
+        />
+        {label && (
+          <span
+            className={cn(
+              "text-slate-700 text-sm font-medium leading-tight",
+              disabled && "text-slate-300",
+            )}
+          >
+            {label}
+          </span>
         )}
-      </div>
-    );
-  },
-);
+      </label>
+      {helpText && (
+        <div className="flex gap-2">
+          <div className={checkboxVariants({ inputSize })} />
+          <span className="text-slate-500 text-sm font-normal">{helpText}</span>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export { Checkbox };
