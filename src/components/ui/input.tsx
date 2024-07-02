05de3dd951f286c2cccd0c8b70f09ec2835a6184
Pawel Skroban <pskrobek@gmail.com>
2024-07-02 11:07:57 +0200

    Extend Input component variants

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,79 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  helpText?: string;
+  suffixIcon?: JSX.Element;
+  prefixIcon?: JSX.Element;
+  hasError?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+  (
+    { className, label, helpText, suffixIcon, prefixIcon, hasError, ...props },
+    ref,
+  ) => {
+    const renderIcon = (icon: JSX.Element, position: "left" | "right") => {
+      return (
+        <span
+          className={cn(
+            "absolute h-[40px] flex items-center",
+            position === "left" && "left-2.5",
+            position === "right" && "right-2.5",
+          )}
+        >
+          {React.cloneElement(icon, {
+            size: "16",
+            className: hasError ? "fill-red-600" : "fill-slate-500",
+          })}
+        </span>
+      );
+    };
 
-export { Input }
+    return (
+      <div className="">
+        <div className="label">
+          <span className=" label-text text-slate-600 text-sm font-medium">{label}</span>
+        </div>
+        <label className="form-control w-full flex relative">
+          {prefixIcon && renderIcon(prefixIcon, "left")}
+          <input
+            type='text'
+            className={cn(
+              "px-3.5 py-2.5 h-[38px] min-h-[38px] rounded-lg gap-2 w-full border-box",
+              "text-sm font-normal",
+              "shadow border border-slate-300 text-black placeholder:text-slate-400",
+              "focus:outline-0 focus:border focus:border-blue-500",
+              "disabled:bg-slate-50 disabled:text-slate-300",
+              prefixIcon && "pl-8",
+              suffixIcon && "pr-8",
+              hasError && "border-red-500 focus:border-red-500",
+              className,
+            )}
+            ref={ref}
+            {...props}
+          />
+          {suffixIcon && renderIcon(suffixIcon, "right")}
+        </label>
+        {helpText && (
+          <div className="label">
+            <span
+              className={cn(
+                "label-text-alt text-slate-500 text-xs font-normal",
+                hasError && "text-red-600",
+              )}
+            >
+              {helpText}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
