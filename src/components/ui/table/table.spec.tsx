3eddc1fee34620b4f11d0ab782b68aba9382d0a3
Pawel Skroban <pskrobek@gmail.com>
2024-07-04 10:15:56 +0200

    Test package with example & pre-commit test check

---
+++ src/components/ui/table/table.spec.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react'
+import { expect, it } from "vitest";
+
+import {
+  Table,
+  TableHeader,
+  TableCaption,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "./index";
+
+it("Table Snaphot", () => {
+  const result = render(
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead info={<span>Info</span>}>Carflow name</TableHead>
+          <TableHead sortable={true}>Version</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {[...Array(10).keys()].map((i) => (
+          <TableRow key={i}>
+            <TableCell>Care Flow Name-{i}</TableCell>
+            <TableCell supportingText="by example@awellhealth.com">
+              Example Cell
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableCaption className="py-0">Table Caption</TableCaption>
+    </Table>,
+  );
+
+  expect(result).toMatchSnapshot();
+});
