449f16e4ea399a1fd9e410d561428ab252a34eb5
Pawel Skroban <pskrobek@gmail.com>
2024-07-10 08:57:02 +0200

    Extend tab item type

--- src/components/ui/tab/types.ts
+++ src/components/ui/tab/types.ts
@@ -1,6 +1,8 @@
+import { ReactElement } from 'react';
+
 export interface TabItem {
   id: string;
-  label: string;
+  label: string | JSX.Element | ReactElement;
   onClick: (item: TabItem) => void;
   className?: string;
 }
