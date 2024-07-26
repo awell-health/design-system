a5a9e8269df9fde17cda05dac9d8b42b485e2e49
Pawel Skroban <pskrobek@gmail.com>
2024-07-26 08:57:01 +0200

    Fix menu element spacing

--- src/components/ui/menu/Menu.tsx
+++ src/components/ui/menu/Menu.tsx
@@ -12,10 +12,10 @@ function Menu({ className, items, ...props }: Props) {
 
     const itemClassNames = cn(
       'group flex box-border',
-      'h-9 pl-2 pr-3 py-2 rounded-md text-sm font-medium text-slate-700',
+      'h-9 pl-2 pr-3 py-2 mb-1 rounded-md text-sm font-medium text-slate-700',
       'hover:border-blue-100 hover:bg-blue-100 hover:text-blue-700',
       'active:!border-blue-100 active:!bg-blue-50 active:!text-blue-700',
-      active && 'border-blue-100 bg-blue-50 text-blue-700',
+      active && 'border border-blue-100 bg-blue-50 text-blue-700',
       !icon && 'ml-3',
       className
     );
