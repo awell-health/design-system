5f9cf37783f58a66df7f267a13155b66fa23eb33
Pawel Skroban <pskrobek@gmail.com>
2024-08-26 18:50:31 +0200

    Add icon onClick prop

--- src/components/ui/icon/icon.tsx
+++ src/components/ui/icon/icon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { ICONS, IconSize, IconType } from './types';
 
 interface Props {
@@ -6,14 +7,21 @@ interface Props {
   size?: IconSize;
   color?: string;
   className?: string;
+  onClick?: () => void;
 }
 
 const Icon = (props: Props): React.JSX.Element => {
-  const { icon, color = 'default', className = 'inline', size = IconSize.m } = props;
+  const {
+    icon,
+    color = 'default',
+    className = 'inline',
+    size = IconSize.m,
+    onClick = undefined
+  } = props;
 
   const IconComponent = ICONS[icon];
 
-  return <IconComponent size={size} color={color} className={className} />;
+  return <IconComponent size={size} color={color} className={className} onClick={onClick} />;
 };
 
 export { Icon };
