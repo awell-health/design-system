ec8ac9c07f7d659525e822d6d50cf0614d01a638
Pawel Skroban <pskrobek@gmail.com>
2024-11-20 13:46:15 +0100

    Inherit input types for component

--- src/components/ui/form/input/input.tsx
+++ src/components/ui/form/input/input.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-interface InputProps {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   helpText?: string;
   suffixIcon?: JSX.Element;
