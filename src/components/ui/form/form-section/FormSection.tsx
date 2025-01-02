63f071d3b278baa772e18701aff06b195583d381
Pawel Skroban <pskrobek@gmail.com>
2025-01-02 16:57:49 +0100

    Fix gap on form section component

--- src/components/ui/form/form-section/FormSection.tsx
+++ src/components/ui/form/form-section/FormSection.tsx
@@ -9,8 +9,10 @@ interface Props {
 export const FormSection: FC<Props> = ({ children, title, hint = null }) => {
   return (
     <div className='py-6 flex flex-col gap-2'>
-      <h5 className='text-lg font-medium leading-5 text-neutral-dark-800'>{title}</h5>
-      {hint !== null && <div className='text-gray-500'>{hint}</div>}
+      <div className='flex flex-col gap-1'>
+        <h5 className='text-lg font-medium leading-5 text-neutral-dark-800'>{title}</h5>
+        {hint !== null && <div className='text-gray-500'>{hint}</div>}
+      </div>
       <div className='mt-2'>{children}</div>
     </div>
   );
