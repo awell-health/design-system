ecc1b7e8c7493fc2f994493ee21d588750580de7
Pawel Skroban <pskrobek@gmail.com>
2025-04-23 17:36:44 +0200

    Export label component

--- src/components/ui/form/label/label.spec.tsx
+++ src/components/ui/form/label/label.spec.tsx
@@ -4,9 +4,19 @@ import React from 'react';
 import { Label } from './label';
 
 describe('Label', () => {
-  it('should render', () => {
+  it('match snapshot', () => {
+    const { container } = render(<Label label='Label' sublabel='Sublabel' />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render label and sublabel', () => {
     render(<Label label='Label' sublabel='Sublabel' />);
     expect(screen.getByText('Label')).toBeInTheDocument();
     expect(screen.getByText('Sublabel')).toBeInTheDocument();
+  });
+
+  it('should render asterisk when required', () => {
+    render(<Label label='Label' sublabel='Sublabel' required />);
+    expect(screen.getByText('*')).toBeInTheDocument();
   });
 });
